845fa64d81e3356613b1a46f52720a3bbe04109a
Kyoorim <kyoorim88@gmail.com>
2023-02-10 12:14:02 +0900

    hotjar 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import { hotjar } from "react-hotjar";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    hotjar.initialize(3341604, 6);
+  }, []);
   return <Component {...pageProps} />;
 }
