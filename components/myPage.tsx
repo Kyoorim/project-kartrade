a528617b396a917d09e13132c5f46e17573790d1
Kyoorim <kyoorim88@gmail.com>
2023-02-15 19:14:53 +0900

    my page 내 page path 구현

--- components/myPage.tsx
+++ components/myPage.tsx
@@ -1,20 +1,24 @@
 import styled from "styled-components";
 import Button from "./button";
 import { authService } from "@/firebase";
+import PathBar from "./pathBar";
 
-const MyPage = () => {
+const MyPage: React.FC<{ isLoggedIn: Boolean }> = ({ isLoggedIn }) => {
   const onLogoutClick = async (): Promise<void> => {
     authService.signOut();
     alert("로그아웃 되었습니다");
   };
   return (
-    <LoginContainer>
-      <div>환영합니다</div>
-      <div>WISHLIST</div>
-      <Button type="submit" onClick={onLogoutClick}>
-        LOG OUT
-      </Button>
-    </LoginContainer>
+    <>
+      <PathBar isLoggedIn={isLoggedIn} />
+      <LoginContainer>
+        <div>환영합니다</div>
+        <div>WISHLIST</div>
+        <Button type="submit" onClick={onLogoutClick}>
+          LOG OUT
+        </Button>
+      </LoginContainer>
+    </>
   );
 };
 
